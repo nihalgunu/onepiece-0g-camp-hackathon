1013f549bcaab83c5a35932f43f1ee8cc43f2933
David Slakter <davidslakter2@gmail.com>
2024-06-15 11:31:56 -0700

    added sample app + sample contract

---
+++ Usopp/Usopp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Usopp
+//
+//  Created by David Slakter on 6/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
